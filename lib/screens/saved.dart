3409026ea69d2c2b97f6ea595a5a97add0a492e5
OCodeCyber <demirorhan0555@gmail.com>
2024-06-20 23:18:56 +0200

    ListTile added

--- lib/screens/saved.dart
+++ lib/screens/saved.dart
@@ -46,6 +46,8 @@ class _SavedState extends State<Saved> {
   }
 }*/
 
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:english_dictionary/screens/details.dart';
 import 'package:english_dictionary/services/local_save.dart';
 import 'package:flutter/material.dart';
 
@@ -59,34 +61,40 @@ class Saved extends StatefulWidget {
 class _SavedState extends State<Saved> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Saved Words'),
-      ),
-      body: FutureBuilder<List<Map<dynamic, dynamic>>>(
-        future: getData(),
-        builder: (BuildContext context,
-            AsyncSnapshot<List<Map<dynamic, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(child: Text("Error loading data"));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("No saved words found"));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final word = snapshot.data![index];
-                return ListTile(
-                  title: Text(word["id"]!),
-                  subtitle: Text(word["def"]!),
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      return Scaffold(
+        body: FutureBuilder<List<Map<dynamic, dynamic>>>(
+          future: getData(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<dynamic, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return const Center(child: Text("Error loading data"));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text("No saved words found"));
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final word = snapshot.data![index];
+                  return ListTile(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Details(
+                                theWord: word["id"],
+                              )));
+                    },
+                    title: Text(word["id"]!),
+                    subtitle: Text(word["def"]!),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      );
+    });
   }
 }
